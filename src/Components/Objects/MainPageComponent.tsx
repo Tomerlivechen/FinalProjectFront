cde05064830242252baa09f17208e5f048032eb9
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-10-31 18:18:14 +0200

    title page font change

--- src/Components/Objects/MainPageComponent.tsx
+++ src/Components/Objects/MainPageComponent.tsx
@@ -1,19 +1,19 @@
-import { AppLogo } from "./AppLogo"
+import { AppLogo } from "./AppLogo";
 
 const MainPageComponent = () => {
-
-
-    return (
-        <>
-       
-
-<div className="flex flex-col items-center p-5 justify-start md:min-h-screen">
-        <AppLogo Size="150"/>
-    <div className="p-5 text-4xl">
-    Deinonychus
-    <p className="ml-10"> Connecting dinosaurs, spring chickens, and everyone in between.</p>
-    </div>
-    </div>
-    </>)
-}
-export {MainPageComponent}
+  return (
+    <>
+      <div className="flex flex-col items-center p-5 justify-start md:min-h-screen">
+        <AppLogo Size="150" />
+        <div className="p-5 text-5xl font-bold">
+          Deinonychus
+          <p className="ml-10 text-3xl font-normal">
+            {" "}
+            Connecting dinosaurs, spring chickens, and everyone in between.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export { MainPageComponent };
