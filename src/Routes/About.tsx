e70aa795ac257a33f496a4f6bc86f371ab3bacb9
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-10-31 21:03:03 +0200

    build

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -2,52 +2,75 @@ import { Tooltip } from "react-bootstrap";
 import ElementFrame from "../Constructors/ElementFrame";
 
 function About() {
-  return(
+  return (
     <ElementFrame tailwind="p-6 max-w-5xl mx-auto">
-        <h1 className="text-3xl font-bold mb-4">About This Project</h1>
+      <h1 className="text-3xl font-bold mb-4">About This Project</h1>
+      <p className="mb-4">
+        Welcome to Dr. Tomer Chen's final project for the .NET course at
+        HackerU! This project required developing both the frontend and backend
+        for a web app.
+      </p>
+      <p className="mb-4">
+        The backend was built using ASP.NET API, and the frontend was created
+        using React + TypeScript, styled with Tailwind (with minimal CSS
+        adjustments). The app is hosted on Azure’s free hosting service, so it
+        may be a touch slow at times.
+      </p>{" "}
+      <p className="mb-4">
+        Here's what it offers as a dynamic social media platform:
+      </p>
+      <ul className=" mb-4">
+        <li>
+          <b>User Profiles</b>: Three user tiers: Admin, Power User, and User,
+          each with different permissions.
+        </li>
+        <li>
+          <b>Posts</b>: Users can post on their profiles and within groups.
+        </li>
+        <li>
+          <b>Comments</b>: Add comments to posts and to other comments (with a
+          limit on depth).
+        </li>
+        <li>
+          <b>Social Groups</b>: Join groups tailored to specific interests.
+        </li>
+        <li>
+          <b>Chat System</b>: Enjoy one-on-one conversations with other users.
+        </li>
+        <li>
+          <b>External Image Storage</b>: Images are stored externally using
+          Cloudinary.
+        </li>
+        <li>
+          <b>Password Recovery</b>: A secure password recovery solution via the
+          Identity framework and EmailJS.
+        </li>
+        <li>
+          <b>Responsiveness</b>: The app is fully responsive and compatible with
+          all screen sizes.
+        </li>
+      </ul>
+      <p>
+        Enjoy the experience!
+        <br />
+      </p>
+      <div className="flex">
         <p className="mb-4">
-            Welcome to Dr. Tomer Chen's final project for the .NET course at HackerU! This project required developing both the frontend and backend for a web app.
-        </p>
-        <p className="mb-4">
-            The backend was built using ASP.NET API, and the frontend was created using React + TypeScript, styled with Tailwind (with minimal CSS adjustments).
-            The app is hosted on Azure’s free hosting service, so it may be a touch slow at times.
-            </p> <p className="mb-4">
-             Here's what it offers as a dynamic social media platform:
-        </p>
-        <ul className=" mb-4">
-            <li>
-                <b>User Profiles</b>: Three user tiers Admin, Power User, and User each with different permissions.
-            </li>
-            <li>
-                <b>Posts</b>: Users can post on their profiles and within groups.
-            </li>
-            <li>
-                <b>Comments</b>: Add comments to posts and on other comments (with a limit on depth).
-            </li>
-            <li>
-                <b>Social Groups</b>: Join groups tailored to specific interests.
-            </li>
-            <li>
-                <b>Chat System</b>: Enjoy one-on-one conversations with other users.
-            </li>
-            <li>
-                <b>External Image Storage</b>: Images are stored externally using Cloudinary.
-            </li>
-            <li>
-                <b>Password Recovery</b>: A secure password recovery solution via the Identity framework and EmailJS.
-            </li>
-            <li>
-                <b>Responsiveness</b>: The app is fully responsive and compatible with all screen sizes.
-            </li>
-        </ul>
-        <p>Enjoy the experience!<br/></p>
-        <div className="flex">
-        <p className="mb-4">
-            If you find any bugs, please reach out via chat to the <b>SysAdmin</b> or by email at:ㅤ</p> <Tooltip title="Send Email to Dr. Tomer Che">
-            <a href="mailto:TomerLiveChen@gmail.com" className="font-bold underline decoration-teal-500 decoration-4">TomerLiveChen@gmail.com</a>.</Tooltip>
-             </div>
+          If you find any bugs, please reach out via chat to the <b>SysAdmin</b>{" "}
+          or by email at:ㅤ
+        </p>{" "}
+        <Tooltip title="Send Email to Dr. Tomer Che">
+          <a
+            href="mailto:TomerLiveChen@gmail.com"
+            className="font-bold underline decoration-teal-500 decoration-4"
+          >
+            TomerLiveChen@gmail.com
+          </a>
+          .
+        </Tooltip>
+      </div>
     </ElementFrame>
-);
+  );
 }
 
 export default About;
