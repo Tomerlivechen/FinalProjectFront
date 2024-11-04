32566a6a625abbb653ad32bdda1cdbbea0e78b31
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-04 14:53:28 +0000

    no posts

--- src/Components/Objects/NoMorePosts.tsx
+++ src/Components/Objects/NoMorePosts.tsx
@@ -3,7 +3,7 @@ import { colors } from "../../Constants/Patterns"
 const NoMorePosts =()=>{
 return (<><div>
     <div  className={`rounded-md border-2 px-2 py-2  ${colors.ElementFrame} font-bold w-[25rem] justify-center text-center`}>
-        No More Posts
+        No Posts
         <img src="https://res.cloudinary.com/dhle9hj3n/image/upload/v1730727060/rglcjqapx5a23f80vysf.jpg"></img>
         </div>
     </div></>)
