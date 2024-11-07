67a04b52befe6159710ba4f43345d2018c68399f
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-07 14:46:51 +0000

    added zoom out function for small screens

--- src/Routes/BackGround.tsx
+++ src/Routes/BackGround.tsx
@@ -1,7 +1,25 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode ,useEffect} from "react";
 import { colors } from "../Constants/Patterns";
+  import { useLocation } from 'react-router-dom'
 
 const BackGround: React.FC<{ children: ReactNode }> = ({ children }) => {
+
+const location = useLocation();
+
+  const setMaxZoomOut=() => {
+    const metaTag = document.querySelector('meta[name="viewport"]');
+    if (!metaTag) return;
+    const width = window.innerWidth;
+    const scale = 1 / (width / 320); 
+    const initialScale = Math.min(1, scale); 
+    const maxScale = 5; 
+    metaTag.setAttribute('content', `width=device-width, initial-scale=${initialScale}, maximum-scale=${maxScale}, user-scalable=yes`);
+  }
+    useEffect(() => {
+      setMaxZoomOut();
+    }, [location]); 
+  
+
   return (
     <>
       <div className="relative">
