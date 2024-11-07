dd49f0784c8ab6141497e1a75cbff8ddabc53aa3
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-07 23:11:11 +0200

    run buld

--- src/Routes/Feed.tsx
+++ src/Routes/Feed.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import ResizableFrame from "../Components/Objects/ResizableFrame";
-import { UserTabList } from "../Components/Objects/UserTabList";
 import UserLane from "../Components/UserLane";
 import { PostFrame } from "../Components/PostFrame";
 import { useParams } from "react-router-dom";
@@ -8,18 +7,10 @@ import { IPostDisplay } from "../Models/Interaction";
 import { Posts } from "../Services/post-service";
 import PostView from "../Components/Objects/PostView";
 import { ProfileGroupsList } from "../Components/Objects/ProfileGroupsList";
-import { IAppUserDisplay } from "../Models/UserModels";
-import { Chat } from "../Services/chat-service";
-import { auth } from "../Services/auth-service";
-import { useUser } from "../CustomHooks/useUser";
-import { isEqual } from "lodash";
-import { colors } from "../Constants/Patterns";
-import ClipSpinner from "../Spinners/ClipSpinner";
 import { InteractingUsersLists } from "../Components/InteractingUsersLists";
 
 const Feed = () => {
   const { postId } = useParams();
-  const userContext = useUser();
   const [singularPost, setSingularPost] = useState<IPostDisplay | null>(null);
 
   useEffect(() => {
