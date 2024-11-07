a1146877c91cb0889cbcfdb720f814dae0cb75ae
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-07 23:25:28 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-})
+  base: './',
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      events: 'events',
+      util: 'util/',
+
+    },
+  },
+  optimizeDeps: {
+    include: ['util','events', 'stream-browserify'], 
+    exclude: []
+  },
+});
