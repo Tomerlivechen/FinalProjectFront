507560b15ea8fb059666a0c1e8b4c9466588bd5a
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2025-01-14 22:09:38 +0200

    increasd axios time out limit

--- src/Utils/Axios-Interceptor.tsx
+++ src/Utils/Axios-Interceptor.tsx
@@ -4,6 +4,7 @@ import { SortResponse } from "./AxiosRespons";
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const client = axios.create({
   baseURL: baseUrl,
+  timeout: 10000,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
